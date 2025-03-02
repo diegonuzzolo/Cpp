6e493f3481c8d9b9582bcf9de8da5f32f50acb00
Diego Nuzzolo <nuzzolo27@gmail.com>
2025-03-02 19:39:55 +0100

    first commit

---
+++ build/CMakeFiles/Magma.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Magma.dir/src/main.cpp.o"
+  "CMakeFiles/Magma.dir/src/main.cpp.o.d"
+  "Magma"
+  "Magma.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Magma.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <vulkan/vulkan.hpp>
+#include <GLFW/glfw3.h>
+
+int main() {
+    std::cout << "Hello, Vulkan Engine!" << std::endl;
+    return 0;
+}
